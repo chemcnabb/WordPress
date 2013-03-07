a01cde607954ce27b1b78e6451688023e1366ce0
Nicolás Iglesias <nico@webpolis.com.ar>
2013-03-07 02:37:48 -0300

    make fields req

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -102,67 +102,78 @@ azureTicketsApp.filter('t', [
  * This directive will attach the field to a model applied to the rules defined
  * in modelsmeta.js .
  */
-azureTicketsApp.directive('atfield', [
-		'$compile',
-		function ($compile) {
-			return {
-				restrict : 'E',
-				scope : {
-					atModel : '=ngModel',
-					atLabel : '=label'
-				},
-				link : function ($scope, $element, $attrs) {
-					var m = $attrs.ngModel.split('.')[0];
-					var f = $attrs.ngModel.split('.')[1];
-					var fieldType = BWL.ModelMeta[m][f];
-					var _el, _label = null;
-					var _attr = {
-						placeholder : f,
-						name : m + '[' + f + ']',
-						id : $attrs.ngModel
-					};
-					var isPass = /Password/g.test(f);
+azureTicketsApp
+		.directive(
+				'atfield',
+				[
+						'$compile',
+						function ($compile) {
+							return {
+								restrict : 'E',
+								scope : {
+									atModel : '=ngModel',
+									atLabel : '=label'
+								},
+								link : function ($scope, $element, $attrs) {
+									var m = $attrs.ngModel.split('.')[0];
+									var f = $attrs.ngModel.split('.')[1];
+									var fieldType = BWL.ModelMeta[m][f];
+									var _el, _label = null;
+									var _attr = {
+										placeholder : f,
+										name : m + '[' + f + ']',
+										id : $attrs.ngModel
+									};
+									var isPass = /Password/g.test(f);
+									var _req = angular
+											.isDefined($attrs.ngRequired) ? 'ng-required="true"'
+											: '';
 
-					// set label if defined
-					if (angular.isDefined($attrs.label)) {
-						_label = jQuery('<label />');
-						_label.text('{{atLabel}}');
-						if (angular.isDefined($attrs.labelClass))
-							_label.addClass($attrs.labelClass);
-					}
+									// set label if defined
+									if (angular.isDefined($attrs.label)) {
+										_label = jQuery('<label />');
+										_label.text('{{atLabel}}');
+										if (angular
+												.isDefined($attrs.labelClass))
+											_label.addClass($attrs.labelClass);
+									}
 
-					// set proper element definition
-					if (/^String/g.test(fieldType)) {
-						_attr.type = !isPass ? 'text' : 'password',
-								_el = jQuery('<input />');
-						if (_label !== null)
-							_label.addClass('pull-left');
+									// set proper element definition
+									if (/^String/g.test(fieldType)) {
+										_attr.type = !isPass ? 'text'
+												: 'password',
+												_el = jQuery('<input ' + _req
+														+ '/>');
+										if (_label !== null)
+											_label.addClass('pull-left');
 
-					}
-					if (/^Boolean/g.test(fieldType)) {
-						_attr.type = 'checkbox', _el = jQuery('<input />');
-						if (_label !== null)
-							_label.addClass('pull-right');
-					}
+									}
+									if (/^Boolean/g.test(fieldType)) {
+										_attr.type = 'checkbox',
+												_el = jQuery('<input />');
+										if (_label !== null)
+											_label.addClass('pull-right');
+									}
 
-					// define new element attributes
-					for (p in $attrs) {
-						if (angular.isString($attrs[p])
-								&& [ 'ngModel' ].indexOf(p) === -1)
-							_el.attr(p, $attrs[p]);
-					}
-					for (p in _attr) {
-						_el.attr(p, _attr[p]);
-					}
+									// define new element attributes
+									for (p in $attrs) {
+										if (angular.isString($attrs[p])
+												&& [ 'ngModel', 'ngRequired' ]
+														.indexOf(p) === -1)
+											_el.attr(p, $attrs[p]);
+									}
+									for (p in _attr) {
+										_el.attr(p, _attr[p]);
+									}
 
-					// make new element available
-					_el.attr('ng-model', 'atModel');
+									// make new element available
+									_el.attr('ng-model', 'atModel');
 
-					if (_label !== null)
-						$compile(_label)($scope);
-					$compile(_el)($scope);
+									if (_label !== null)
+										$compile(_label)($scope);
+									$compile(_el)($scope);
 
-					$element.append(_el).append(_label);
-				}
-			}
-		} ]);
+									$element.append(_el).append(_label);
+								}
+							}
+						} ]);
