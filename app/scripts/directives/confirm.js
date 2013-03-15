9927eac8461754f74266e6b82d9e8e4851f695e8
Nicolás Iglesias <nico@webpolis.com.ar>
2013-03-15 13:07:33 -0300

    prevent err on minification

--- app/scripts/directives/confirm.js
+++ app/scripts/directives/confirm.js
@@ -1,7 +1,9 @@
-azureTicketsApp.directive('ngConfirm', function($window) {
-  return function(scope, elem, attrs) {
-    elem.bind('click', function() {
-      return $window.confirm(attrs['ngConfirm']);
-    });
-  };
-});
+azureTicketsApp.directive('ngConfirm', [
+    '$window', function($window) {
+      return function(scope, elem, attrs) {
+        elem.bind('click', function() {
+          return $window.confirm(attrs['ngConfirm']);
+        });
+      };
+    }
+]);
