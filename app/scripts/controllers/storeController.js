dd1cc60a1cb48bac0499101ace416e9b2f47ec7c
Nicolás Iglesias <nico@webpolis.com.ar>
2013-03-20 00:15:36 -0300

    remove unnecesary code

--- app/scripts/controllers/storeController.js
+++ app/scripts/controllers/storeController.js
@@ -3,8 +3,7 @@ function storeController($scope, $cookieStore, $timeout, configService,
     geoService, formService) {
   $scope.storeKey = $cookieStore.get('storeKey') || null,
       $scope.config = configService, $scope.name = 'store', $scope.stores = [],
-      $scope.currencies = [], $scope.countries = [], $scope.continents = [],
-      $scope.regions = [], $scope.paymentProviders = [], $scope.timezones = [],
+      $scope.currencies = [], $scope.paymentProviders = [],
       $scope.wizard = formService.getWizard($scope);
 
   /**
@@ -44,8 +43,6 @@ function storeController($scope, $cookieStore, $timeout, configService,
                                           : null;
                                       $scope.wizard.currentStep = 1;
 
-                                      // $scope.loadContinents();
-                                      //
                                       if ($scope.Store.Address
                                           && $scope.Store.Address.Country !== null) {
                                         // we've got a country, alert address
@@ -117,109 +114,6 @@ function storeController($scope, $cookieStore, $timeout, configService,
       errorService.log(err)
     });
   }
-  //
-  // $scope.loadContinents = function() {
-  // geoService.getContinents().then(function(continents) {
-  // $scope.continents = continents;
-  // }, function(err) {
-  // errorService.log(err)
-  // });
-  // }
-  //
-  // $scope.loadCountriesByContinent = function(address, reset) {
-  // if (angular.isDefined(address.tmpContinentIso)) {
-  // var continentIso = address.tmpContinentIso;
-  //
-  // // reset lists
-  // $scope.countries = [];
-  // $scope.regions = [];
-  // $scope.timezones = [];
-  //
-  // // reset address
-  // if (reset) {
-  // $scope.Country = null;
-  // address.City = null, address.AddressLine1 = null,
-  // address.AddressLine2 = null, address.Timezone = null,
-  // address.Region = null, address.PostalCode = null;
-  // }
-  //
-  // geoService.getCountriesByContinent(continentIso).then(
-  // function(countries) {
-  // $scope.countries = countries;
-  // }, function(err) {
-  // errorService.log(err)
-  // });
-  // }
-  // }
-  //
-  // $scope.loadTimezonesByCountry = function(address) {
-  // if (angular.isDefined(address.Country)) {
-  // geoService.getTimezonesByCountry(address.Country).then(
-  // function(timezones) {
-  // $scope.timezones = timezones;
-  // }, function(err) {
-  // errorService.log(err)
-  // });
-  // }
-  // }
-  //
-  // $scope.loadCountry = function(address) {
-  // if (angular.isDefined(address.Country)) {
-  // geoService.loadCountry(address.Country).then(function(country) {
-  // $scope.Country = country;
-  // address.tmpContinentIso = country.ContinentISO;
-  //
-  // if (!country.HasPostalCodes) {
-  // $scope.loadRegionsByCountry(address)
-  // }
-  // if ($scope.countries.length === 0) {
-  // $scope.loadCountriesByContinent(address)
-  // }
-  // if ($scope.timezones.length === 0) {
-  // $scope.loadTimezonesByCountry(address)
-  // }
-  // }, function(err) {
-  // errorService.log(err)
-  // });
-  // }
-  // }
-  //
-  // $scope.loadRegionsByCountry = function(address) {
-  // if (angular.isDefined(address.Country)) {
-  // geoService.getRegionsByCountry(address.Country).then(function(regions) {
-  // $scope.regions = regions;
-  // }, function(err) {
-  // errorService.log(err)
-  // });
-  // }
-  // }
-  //
-  // $scope.getCityByPostalCode = function(address) {
-  // if (angular.isDefined(address.Country)
-  // && angular.isDefined(address.PostalCode) && address.PostalCode !== null
-  // && address.PostalCode.trim().length >= 3) {
-  // geoService.getCityByPostalCode(address.Country, address.PostalCode).then(
-  // function(city) {
-  // // lookup timezone
-  // geoService.getCityByName(city.CityName, city.RegionISO,
-  // city.CountryISO).then(
-  // function(city) {
-  // if (angular.isDefined(city.Name)
-  // && angular.isDefined(city.RegionISO)) {
-  // address.City = city.Name;
-  // address.Region = city.RegionISO;
-  // address.Timezone = city.TimezoneName;
-  // $scope.loadRegionsByCountry(address);
-  // $scope.loadTimezonesByCountry(address);
-  // }
-  // }, function(err) {
-  // errorService.log(err)
-  // });
-  // }, function(err) {
-  // errorService.log(err)
-  // });
-  // }
-  // }
 
   $scope.loadPaymentProvidersByCurrency = function(currency) {
     if (angular.isDefined(currency) && currency !== '') {
@@ -230,7 +124,6 @@ function storeController($scope, $cookieStore, $timeout, configService,
             errorService.log(err)
           });
     }
-
   }
 
   /**
--- app/scripts/controllers/venueController.js
+++ app/scripts/controllers/venueController.js
@@ -3,7 +3,6 @@ function venueController($scope, $cookieStore, configService, authService,
     formService) {
   $scope.storeKey = $cookieStore.get('storeKey') || null,
       $scope.config = configService, $scope.name = 'venue', $scope.venues = [],
-      $scope.countries = [], $scope.regions = [], $scope.timezones = [],
       $scope.wizard = formService.getWizard($scope);
 
   $scope.$watch('wizard.open', function(v) {
@@ -68,109 +67,6 @@ function venueController($scope, $cookieStore, configService, authService,
     $scope.wizard.saved = false;
     $scope.wizard.finished = false;
     $scope.wizard.currentStep = 1
-  }
-
-  $scope.loadContinents = function() {
-    geoService.getContinents().then(function(continents) {
-      $scope.continents = continents;
-    }, function(err) {
-      errorService.log(err)
-    });
-  }
-
-  $scope.loadCountriesByContinent = function(address, reset) {
-    if (angular.isDefined(address.tmpContinentIso)) {
-      var continentIso = address.tmpContinentIso;
-
-      // reset lists
-      $scope.countries = [];
-      $scope.regions = [];
-      $scope.timezones = [];
-
-      // reset address
-      if (reset) {
-        $scope.Country = null;
-        address.City = null, address.AddressLine1 = null,
-            address.AddressLine2 = null, address.Timezone = null,
-            address.Region = null, address.PostalCode = null;
-      }
-
-      geoService.getCountriesByContinent(continentIso).then(
-          function(countries) {
-            $scope.countries = countries;
-          }, function(err) {
-            errorService.log(err)
-          });
-    }
-  }
-
-  $scope.loadTimezonesByCountry = function(address) {
-    if (angular.isDefined(address.Country)) {
-      geoService.getTimezonesByCountry(address.Country).then(
-          function(timezones) {
-            $scope.timezones = timezones;
-          }, function(err) {
-            errorService.log(err)
-          });
-    }
-  }
-
-  $scope.loadCountry = function(address) {
-    if (angular.isDefined(address.Country)) {
-      geoService.loadCountry(address.Country).then(function(country) {
-        $scope.Country = country;
-        address.tmpContinentIso = country.ContinentISO;
-
-        if (!country.HasPostalCodes) {
-          $scope.loadRegionsByCountry(address)
-        }
-        if ($scope.countries.length === 0) {
-          $scope.loadCountriesByContinent(address)
-        }
-        if ($scope.timezones.length === 0) {
-          $scope.loadTimezonesByCountry(address)
-        }
-      }, function(err) {
-        errorService.log(err)
-      });
-    }
-  }
-
-  $scope.loadRegionsByCountry = function(address) {
-    if (angular.isDefined(address.Country)) {
-      geoService.getRegionsByCountry(address.Country).then(function(regions) {
-        $scope.regions = regions;
-      }, function(err) {
-        errorService.log(err)
-      });
-    }
-  }
-
-  $scope.getCityByPostalCode = function(address) {
-    if (angular.isDefined(address.Country)
-        && angular.isDefined(address.PostalCode) && address.PostalCode !== null
-        && address.PostalCode.trim().length >= 3) {
-      geoService.getCityByPostalCode(address.Country, address.PostalCode).then(
-          function(city) {
-            // lookup timezone
-            geoService.getCityByName(city.CityName, city.RegionISO,
-                city.CountryISO).then(
-                function(city) {
-                  if (angular.isDefined(city.Name)
-                      && angular.isDefined(city.RegionISO)) {
-                    address.City = city.Name;
-                    address.Region = city.RegionISO;
-                    address.Timezone = city.TimezoneName;
-                    $scope.loadRegionsByCountry(address);
-                    $scope.loadTimezonesByCountry(address);
-                  }
-                }, function(err) {
-                  errorService.log(err)
-                });
-          }, function(err) {
-            errorService.log(err)
-          });
-    }
   }
 
   $scope.save = function() {
